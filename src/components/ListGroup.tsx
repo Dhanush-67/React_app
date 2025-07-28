e5ac59a978140cf841b9eb24e6019530b708bc42
Zeeton <146897841+Dhanush-67@users.noreply.github.com>
2025-07-28 13:58:59 -0400

    Added a dynamic list component

---
+++ src/components/ListGroup.tsx
@@ -0,0 +1,16 @@
+function ListGroup() {
+  const items = ["New York", "San Francisco", "Tokyo", "Paris", "London"];
+
+  return (
+    <>
+      <h1>List</h1>
+      <ul className="list-group">
+        {items.map((item) => (
+          <li>{item}</li>
+        ))}
+      </ul>
+    </>
+  );
+}
+
+export default ListGroup;
